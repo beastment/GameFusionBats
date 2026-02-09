e6696bbec7ed2f0e596adb75c997a7b4a0d9fe49
beastment <ben.eastment@gmail.com>
2026-02-09 21:22:04 +1100

    Implement profile file copy before exit
    Added functionality to copy profile files before exiting.

--- helper.bat
+++ helper.bat
@@ -27,7 +27,7 @@ rename "%REALFILE%" "%ORIGFILE%"
 :: Launch AntiMicroX and wait
 start "" "D:\antimicrox-3.5.1-PortableWindows-AMD64\bin\antimicrox.exe"
 
-:: Short delay to ensurer antimicrox to starts first
+:: Short delay to ensure antimicrox starts first
 ping -n 3 127.0.0.1>NUL
 
 :: Run the game and wait until it exits
@@ -38,5 +38,15 @@ rename "%ORIGFILE%" "%REALFILE%"
 rename exebat.exe "%ORIGFILE%"
 
 taskkill /F /IM antimicrox.exe
+
+:: --- NEW: copy profile files before exit ---
+copy /Y "C:\GH-Default Profile.amgp" "C:\!BASENAME!.amgp" >NUL
+
+if not exist "D:\antimicrox-3.5.1-PortableWindows-AMD64\profiles" (
+    mkdir "D:\antimicrox-3.5.1-PortableWindows-AMD64\profiles" >NUL 2>&1
+)
+
+copy /Y "C:\!BASENAME!.amgp" "D:\antimicrox-3.5.1-PortableWindows-AMD64\profiles\!BASENAME!.amgp" >NUL
+:: --- end NEW ---
 
 exit
